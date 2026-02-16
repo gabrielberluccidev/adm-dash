270bfd5bd6786bd122edd2415fd31130bbe9fe0c
gabrielberlucci <gabrielberluccidev@proton.me>
2026-02-16 20:40:28 -0300

    feat: add sidebar, containt header and content

---
+++ src/components/layout/sidebar.tsx
@@ -0,0 +1,67 @@
+import type { ReactNode } from 'react';
+
+interface SideBarRootProps {
+  children: ReactNode;
+}
+
+export const SideBarRoot = ({ children }: SideBarRootProps) => {
+  return <div className="bg-[#1A1C2E] h-dvh w-80 p-6">{children}</div>;
+};
+
+interface SideBarHeaderRootProps {
+  children: ReactNode;
+}
+
+export const SideBarHeaderRoot = ({ children }: SideBarHeaderRootProps) => {
+  return <div className="ml-4">{children}</div>;
+};
+
+interface SideBarTitleProps {
+  children: ReactNode;
+}
+
+export const SideBarTitle = ({ children }: SideBarTitleProps) => {
+  return <h1 className="text-slate-50 font-semibold text-2xl">{children}</h1>;
+};
+
+interface SideBarSubTitleProps {
+  children: ReactNode;
+}
+
+export const SideBarSubTitle = ({ children }: SideBarSubTitleProps) => {
+  return <p className="text-slate-400 text-sm">{children}</p>;
+};
+
+interface SideBarContentProps {
+  children: ReactNode;
+}
+
+export const SideBarContent = ({ children }: SideBarContentProps) => {
+  return <div className="flex items-center">{children}</div>;
+};
+
+interface SideBarContentWrapperProps {
+  children: ReactNode;
+}
+
+export const SideBarContentWrapper = ({
+  children,
+}: SideBarContentWrapperProps) => {
+  return <div className="">{children}</div>;
+};
+
+interface SideBarFooterProps {
+  children: ReactNode;
+}
+
+export const SideBarFooter = ({ children }: SideBarFooterProps) => {
+  return <div>{children}</div>;
+};
+
+interface SideBarLabelProps {
+  children: ReactNode;
+}
+
+export const SideBarLabel = ({ children }: SideBarLabelProps) => {
+  return <p className="text-xs text-slate-400 ml-4 mt-10">{children}</p>;
+};
